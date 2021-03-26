b5c2a7b6cb015ddb2a33cc337e41e50142afed6b
Alireza Jafari <alirezaja1384@gmail.com>
2021-03-26 14:17:19 +0430

    Blog renamed to learning

--- src/core/settings.py
+++ src/core/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'authentication.apps.AuthenticationConfig',
-    'blog.apps.BlogConfig'
+    'learning.apps.LearningConfig'
 ]
 
 MIDDLEWARE = [
