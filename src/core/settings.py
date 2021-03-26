de20a87201c148016f1b0cc917fd410115aa9a95
Alireza Jafari <alirezaja1384@gmail.com>
2021-03-27 00:13:32 +0430

    SECRET_KEY changed and django will read it from environment variables

--- src/core/settings.py
+++ src/core/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')otx_8pz$gt@iq&@eqda$4ut1w3y3#798fl)*2c0wsxr6*4!oi'
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
