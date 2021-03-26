9d5456ce78ef2439ac73c72286e82cc832504531
Alireza Jafari <alirezaja1384@gmail.com>
2021-03-26 16:03:04 +0430

    Category model created

---
+++ src/learning/models/category.py
@@ -0,0 +1,16 @@
+" Grade model "
+from django.db import models
+
+
+class Category(models.Model):
+    """ Grade model """
+
+    parent_grade = models.ForeignKey(
+        'self', on_delete=models.SET_NULL, null=True, blank=True,
+        related_name='child_grades', verbose_name='پایه والد')
+
+    name = models.CharField('نام', max_length=30)
+
+    slug = models.SlugField('اسلاگ', max_length=50, allow_unicode=True)
+
+    is_active = models.BooleanField('فعال', default=True)
