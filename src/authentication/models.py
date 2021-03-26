ab39b1223a7060cbde818a7eb7198016e01a7aa1
Alireza Jafari <alirezaja1384@gmail.com>
2021-03-26 13:47:52 +0430

    Custom user model registred

--- src/authentication/models.py
+++ src/authentication/models.py
@@ -1,3 +1,17 @@
+"""
+    Authentication models
+"""
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    " Customized user model "
+
+    avatar = models.ImageField("تصویر پروفایل", upload_to="Images/Avatars")
+
+    scores = models.IntegerField("امتیاز")
+
+    coins = models.IntegerField("سکه ها")
+
+    diamonds = models.IntegerField("الماس ها")
--- src/core/settings.py
+++ src/core/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'authentication.apps.AuthenticationConfig',
+    'blog.apps.BlogConfig'
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+AUTH_USER_MODEL = 'authentication.User'
