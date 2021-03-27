25bb3b0395e9a21696e3412083e5f5304b1c202b
Alireza Jafari <alirezaja1384@gmail.com>
2021-03-27 16:06:17 +0430

    Custom user model registed in admin panel and avatar field added to its fieldset

--- src/authentication/admin.py
+++ src/authentication/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.admin.decorators import register
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from .models import User
+
+
+@register(User)
+class UserAdmin(BaseUserAdmin):
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ("پروفایل",{"fields":('avatar', )}),
+    )
