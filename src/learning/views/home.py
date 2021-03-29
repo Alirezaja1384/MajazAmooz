66151146fa138db9f8baa0ee9eb3c51e7970bb61
Alireza Jafari <alirezaja1384@gmail.com>
2021-03-29 16:22:02 +0430

    Home view => unused HttpResponse deleted, home_view docstring added, module docstring typo mistake fixed

--- src/learning/views/home.py
+++ src/learning/views/home.py
@@ -1,6 +1,7 @@
-""" Lean home view """
-from django.http import HttpRequest, HttpResponse
+""" Learn home view """
+from django.http import HttpRequest
 from django.shortcuts import render
 
 def home_view(request: HttpRequest):
+    """ Home view """
     return render(request, 'learning/home.html')
