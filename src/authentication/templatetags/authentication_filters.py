97b3e7772e32d7851b50854be6ce396ddbdeb03d
Alireza Jafari <alirezaja1384@gmail.com>
2021-04-03 23:39:49 +0430

    authentication templatetags => authentication_filters (includes full_name filter) added

---
+++ src/authentication/templatetags/authentication_filters.py
@@ -0,0 +1,15 @@
+""" Authentication template filters """
+from django import template
+
+from authentication.models import User
+
+register = template.Library()
+
+
+@register.filter
+def full_name(user: User):
+    """
+    @param user: User instance
+    @return: user's full name
+    """
+    return f"{user.first_name} {user.last_name}"
