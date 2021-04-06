79736fff8ad780a41e32b8bb18fbe86985773c40
Alireza Jafari <alirezaja1384@gmail.com>
2021-04-06 13:03:21 +0430

    utilities project created and registered in core project
    image_utils templatetag created and url filter created

---
+++ src/utilities/templatetags/imge_utils.py
@@ -0,0 +1,23 @@
+"""
+    Image utility templatetags
+"""
+from django import template
+from django.db.models import ImageField
+from django.templatetags.static import static
+
+register = template.Library()
+
+
+@register.filter
+def url(image: ImageField) -> str:
+    """
+    Returns ImageField's url if exist
+    else returns not-found image
+
+    @param image: image
+    @return: image url
+    """
+    try:
+        return image.url
+    except ValueError:
+        return static('img/not-found.png')
