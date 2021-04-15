64901a257f3dcbef2bca5d37eb8ed4eed4a3e7c4
Alireza Jafari <alirezaja1384@gmail.com>
2021-04-15 11:50:18 +0430

    tutorial.py get_related_tutorials =>
     * while loop less or equal changed to less
    
     * get_tutorial_by_categories() tutorial_count changed to tutorial_count - len(related_tutorials)
    
     * related_tutorials distinct method changed

--- src/learning/views/tutorial.py
+++ src/learning/views/tutorial.py
@@ -40,8 +40,8 @@ def get_related_tutorials(tutorial: Tutorial, fields: tuple, tutorial_count: int
     # there isn't any category in list
     # or all of categories are not None
     # (when all categories are none search will end)
-    while len(related_tutorials) <= tutorial_count and (not categories or
-                                                        all(map(lambda cat: cat is not None, categories))):
+    while len(related_tutorials) < tutorial_count and (not categories or
+                                                       all(map(lambda cat: cat is not None, categories))):
         # when categories has at least one item
         if len(categories) > 0:
             # replace categories by their parent categories
@@ -56,15 +56,16 @@ def get_related_tutorials(tutorial: Tutorial, fields: tuple, tutorial_count: int
         not_none_categories = list(filter(lambda cat: cat is not None, categories))
 
         # adds founded tutorials to related_tutorials list
-        related_tutorials += get_tutorial_by_categories(not_none_categories, fields, tutorial.id, tutorial_count)
+        related_tutorials += get_tutorial_by_categories(not_none_categories, fields, tutorial.id,
+                                                        tutorial_count - len(related_tutorials))
 
         # distinct related_tutorials
-        related_tutorials = list(set(related_tutorials))
+        related_tutorials = list(dict.fromkeys(related_tutorials))
 
     return related_tutorials
 
 
-@cache_page(timeout=60*15)
+@cache_page(timeout=60 * 15)
 def tutorial_details_view(request: HttpRequest, slug: str):
     """ Tutorial details view """
     tutorial = get_object_or_404(Tutorial.objects.filter(
