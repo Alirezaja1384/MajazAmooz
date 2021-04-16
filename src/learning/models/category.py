3c9010c85d7a013c3771f90ba1a249f6d59b8b49
Alireza Jafari <alirezaja1384@gmail.com>
2021-04-16 17:56:50 +0430

    category model =>  * name and slug made unique  * slug blank=True  * slugify on save added
    tutorial model =>
     * title and slug made unique
     * slug blank=True and last_edit_date blank,null=True
     * last_edit_date auto_now deleted
     * is_edited default set False
     * set is_edited True and last_edit_date current date on edit
     * slugify on save
    
    tutorial comment model =>
     * last_edit_date blank,null=True
     * last_edit_date auto_now deleted
     * set is_edited True and last_edit_date current date on edit

--- src/learning/models/category.py
+++ src/learning/models/category.py
@@ -1,19 +1,27 @@
-" Grade model "
+""" Category mode """
 from django.db import models
+from django.utils.text import slugify
+
+from django_lifecycle import LifecycleModel, hook, BEFORE_SAVE
 
 
-class Category(models.Model):
-    """ Grade model """
+class Category(LifecycleModel):
+    """ Category model """
 
     parent_category = models.ForeignKey(
         'self', on_delete=models.SET_NULL, null=True, blank=True,
         related_name='child_categories', verbose_name='پایه والد')
 
-    name = models.CharField('نام', max_length=30)
+    name = models.CharField('نام', unique=True, max_length=30)
 
-    slug = models.SlugField('اسلاگ', max_length=50, allow_unicode=True)
+    slug = models.SlugField('اسلاگ', unique=True, max_length=50,
+                            blank=True, allow_unicode=True)
 
     is_active = models.BooleanField('فعال', default=True)
+
+    @hook(BEFORE_SAVE)
+    def on_save(self):
+        self.slug = slugify(self.name, allow_unicode=True)
 
     class Meta:
         verbose_name = 'دسته بندی'
