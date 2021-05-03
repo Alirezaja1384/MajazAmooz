e636e2770546df5578efd7eb02717efb0b4eb5bc
Alireza Jafari <alirezaja1384@gmail.com>
2021-05-03 15:12:56 +0430

    Django debug toolbar installed

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,18 +1,5 @@
-"""core URL Configuration
+import debug_toolbar
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('learning/', include('learning.urls'))
-"""
 from django.contrib import admin
 from django.urls import (path, include)
 from django.conf import settings
@@ -22,4 +9,8 @@ urlpatterns = [
     path('', include('learning.urls')),
     path('admin/', admin.site.urls),
     path('ajax/', include('ajax.urls')),
+
+    # debug-toolbar
+    path('__debug__/', include(debug_toolbar.urls)),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
