0fe78b982e124c40470a48b6c0220a6809c9ef0e
Alireza Jafari <alirezaja1384@gmail.com>
2021-05-08 18:29:48 +0430

    LogoutRequiredMixin => user-friendly exception when LOGOUT_REQUIRED_URL didn't set

--- src/utilities/view_utilities/logout_required.py
+++ src/utilities/view_utilities/logout_required.py
@@ -13,8 +13,17 @@ class LogoutRequiredMixin():
 
         # If user was authenticated redirect
         # to logout required url
+        try:
+            logout_required_url = settings.LOGOUT_REQUIRED_URL
+        except AttributeError as ex:
+            msg = ("You must specify LOGOUT_REQUIRED_URL in your "
+                   "settings module to use LogoutRequiredMixin")
+
+            raise AttributeError(msg) from ex
+
+
         if request.user.is_authenticated:
             next_url = request.GET.get('next') or request.path
-            return redirect(settings.LOGOUT_REQUIRED_URL + "?next=" + next_url)
+            return redirect(logout_required_url + "?next=" + next_url)
 
         return super().dispatch(request, *args, **kwargs)
