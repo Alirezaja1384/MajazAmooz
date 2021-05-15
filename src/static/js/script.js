6013b16e267c89bbea58534bd2998f7411a36e59
Alireza Jafari <alirezaja1384@gmail.com>
2021-05-15 15:59:27 +0430

    Tutorial page related tutorials carousel item count reduced

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -1,6 +1,32 @@
 $(document).ready(() => {
     $('[data-toggle="tooltip"]').tooltip();
 });
+
+$('.owl-carousel.owl-carousel-sm').owlCarousel({
+    rtl: true,
+    loop: false,
+    rewind: true,
+    margin: 10,
+    nav: true,
+    autoplay: true,
+    autoplayHoverPause: true,
+    // autoHeight: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        575: {
+            items: 2
+        },
+        992: {
+            items: 2
+        },
+        1200: {
+            items: 3
+        }
+    }
+});
+
 $('.owl-carousel').owlCarousel({
     rtl: true,
     loop: false,
