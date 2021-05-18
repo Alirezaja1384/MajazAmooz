98c7b048accd15861a5fc4c56685823bae54bc76
Alireza Jafari <alirezaja1384@gmail.com>
2021-05-18 13:07:48 +0430

    Category model parent_category verbose name changed and checking parent category's id and id are not same added

--- src/learning/models/category.py
+++ src/learning/models/category.py
@@ -1,6 +1,7 @@
 """ Category mode """
 from django.db import models
 from django.utils.text import slugify
+from django.core.exceptions import ValidationError
 
 from django_lifecycle import LifecycleModel, hook, BEFORE_SAVE
 
@@ -12,7 +13,7 @@ class Category(LifecycleModel):
 
     parent_category = models.ForeignKey(
         'self', on_delete=models.SET_NULL, null=True, blank=True,
-        related_name='child_categories', verbose_name='پایه والد')
+        related_name='child_categories', verbose_name='دسته بندی والد')
 
     name = models.CharField('نام', unique=True, max_length=30)
 
@@ -21,16 +22,22 @@ class Category(LifecycleModel):
 
     is_active = models.BooleanField('فعال', default=True)
 
+    # Validate data (for admin panel)
+    def clean(self):
+        if self.id == self.parent_category_id:
+            raise ValidationError(
+                'والد نمی تواند با دسته بندی فعلی یکسان باشد')
+
     @hook(BEFORE_SAVE)
     def on_save(self):
         self.slug = slugify(self.name, allow_unicode=True)
 
+    def __str__(self):
+        return self.name
+
     class Meta:
         verbose_name = 'دسته بندی'
         verbose_name_plural = 'دسته بندی ها'
-
-    def __str__(self):
-        return self.name
 
     # Custom queryset
     objects = CategoryQueryset.as_manager()
