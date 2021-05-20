390632318a724cf9e5fe3c940c722fa708f51c38
Alireza Jafari <alirezaja1384@gmail.com>
2021-05-20 21:30:22 +0430

    active_confirmed_tutorials queryset function added

---
+++ src/learning/querysets/tutorial_user_relation_querysets.py
@@ -0,0 +1,13 @@
+from django.db.models import QuerySet
+
+from utilities.model_utils import ConfirmStatusChoices
+
+
+class TutorialUserRelationQuerySet(QuerySet):
+    def active_confirmed_tutorials(self) -> QuerySet:
+        """
+        Returns:
+            [QuerySet]: objects with active and confirmed tutorial
+        """
+        return self.filter(tutorial__is_active=True,
+                           tutorial__confirm_status=ConfirmStatusChoices.CONFIRMED)
