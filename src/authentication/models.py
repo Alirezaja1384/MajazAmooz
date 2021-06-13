585aa02250a7b5fdaa3c88fa04ad983810112d9a
Alireza Jafari <alirezaja1384@gmail.com>
2021-06-13 17:54:42 +0430

    ImageFields replaced by ResizedImageField

--- src/authentication/models.py
+++ src/authentication/models.py
@@ -1,26 +1,27 @@
-"""
+'''
     Authentication models
-"""
+'''
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
+from django_resized import ResizedImageField
 
 
 class User(AbstractUser):
-    " Customized user model "
+    """ Customized user model """
 
     email = models.EmailField(max_length=254, unique=True,
                               verbose_name='ایمیل')
 
-    avatar = models.ImageField(
-        "تصویر پروفایل", upload_to="images/avatars",
-        default="default/authentication/user-avatar.png")
+    avatar = ResizedImageField(
+        upload_to='images/avatars', default='default/authentication/user-avatar.png',
+        size=[300, 300], crop=['middle', 'center'], verbose_name='تصویر پروفایل')
 
-    scores = models.IntegerField("امتیاز", default=0)
+    scores = models.IntegerField('امتیاز', default=0)
 
-    coins = models.IntegerField("سکه ها", default=0)
+    coins = models.IntegerField('سکه ها', default=0)
 
-    diamonds = models.PositiveIntegerField("الماس ها", default=0)
+    diamonds = models.PositiveIntegerField('الماس ها', default=0)
 
     email_confirmed = models.BooleanField(
         verbose_name='تایید ایمیل', default=False)
