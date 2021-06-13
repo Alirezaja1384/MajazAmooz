8947ff8eca50015b682375f21e05d447745b9905
Alireza Jafari <alirezaja1384@gmail.com>
2021-06-14 00:23:09 +0430

    image_tag template tag implemented

--- src/utilities/templatetags/image_utils.py
+++ src/utilities/templatetags/image_utils.py
@@ -4,6 +4,7 @@
 from django import template
 from django.db.models import ImageField
 from django.templatetags.static import static
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
@@ -21,3 +22,25 @@ def image_url(image: ImageField) -> str:
         return image.url
     except (ValueError, AttributeError):
         return static('img/not-found.png')
+
+
+@register.simple_tag
+def image_tag(image: ImageField, alt: str = '', max_width: int = 250, max_height: int = 250) -> str:
+    """ Returns ImageField's image as a img tag if image exists
+        else returns not-found image
+
+    Args:
+        image (ImageField): Image to create img tag
+        alt (str, optional): Alternative text for image. Defaults to ''.
+        max_width (int, optional): Max width of img tag (pixels). Defaults to 250.
+        max_height (int, optional): Max height of img tag (pixels). Defaults to 250.
+
+    Returns:
+        str: img tag as a safe string
+    """
+    url = image_url(image)
+    tag = (
+        f'<a href="{url}"><img src="{url}" alt="{alt}" class="img-thumbnail"'
+        f'style="max-width: {max_width}px; max-height: {max_height}px;"/></a>'
+    )
+    return mark_safe(tag)
