03735ee00b4e5584818cf15f3c691332e51a565f
Alireza Jafari <alirezaja1384@gmail.com>
2021-06-14 00:49:18 +0430

    User avatar added to UserAdmin's list_display

--- src/authentication/admin.py
+++ src/authentication/admin.py
@@ -1,8 +1,8 @@
-from django.contrib import admin
 from django.contrib.admin.decorators import register
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-
 from django.contrib.auth import get_user_model
+from authentication.models import User
+from utilities.templatetags.image_utils import image_tag
 
 
 UserModel = get_user_model()
@@ -10,6 +10,14 @@ UserModel = get_user_model()
 
 @register(UserModel)
 class UserAdmin(BaseUserAdmin):
+
+    def avatar_image_tag(self, obj: User):
+        return image_tag(obj.avatar, str(obj), 50, 50, additional_styles='border-radius:50%;')
+    avatar_image_tag.short_description = 'تصویر پروفایل'
+
+    list_display = ('avatar_image_tag', 'username',
+                    'email', 'first_name', 'last_name',)
+
     fieldsets = BaseUserAdmin.fieldsets + (
-        ("سایر",{"fields":('avatar', 'email_confirmed', )}),
+        ('سایر', {'fields': ('avatar', 'email_confirmed', )}),
     )
