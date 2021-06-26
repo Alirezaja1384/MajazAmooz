561df6fa5b6b86aa7baeb92e69f76bd66d196602
Alireza Jafari <alirezaja1384@gmail.com>
2021-06-26 11:10:12 +0430

    TimezoneMiddleware tests added

--- src/shared/tests/test_middlewares.py
+++ src/shared/tests/test_middlewares.py
@@ -30,5 +30,27 @@ class LoginRequiredMiddlewareTest(TestCase):
         self.assertNotIn(response.status_code, [301, 302])
 
     def test_should_not_redirect_non_login_required(self):
-        response = self.client.get('/')
+        response = self.client.get("/")
         self.assertNotIn(response.status_code, [301, 302])
+
+
+@override_settings(DEFAULT_USER_TZ="Australia/Eucla")
+class TimezoneMiddlewareTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username="testusername")
+
+    def test_use_default_user_tz(self):
+        response = self.client.get("/")
+        self.assertEqual(response.headers.get("Time-Zone"), "Australia/Eucla")
+
+    def test_use_user_session(self):
+        # To modify the session and then save it, it must be stored
+        # in a variable first (because a new SessionStore is created
+        # every time this property is accessed)
+        # https://docs.djangoproject.com/en/stable/topics/testing/tools/#django.test.Client.session
+        session = self.client.session
+        session["timezone"] = "America/Antigua"
+        session.save()
+
+        response = self.client.get("/")
+        self.assertEqual(response.headers.get("Time-Zone"), "America/Antigua")
