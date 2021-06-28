ccbf1d1deef8d88e260eca386273cd011913c567
Alireza Jafari <alirezaja1384@gmail.com>
2021-06-28 23:52:51 +0430

    Login view tests implemented

--- src/authentication/tests/test_views.py
+++ src/authentication/tests/test_views.py
@@ -1,7 +1,11 @@
 from django.test import TestCase
 from django.shortcuts import reverse
+from django.contrib.auth import get_user_model
 from django.contrib.messages import get_messages
-from authentication.forms import RegisterForm
+from model_bakery import baker
+from authentication.forms import RegisterForm, LoginForm
+
+User = get_user_model()
 
 
 class RegisterTest(TestCase):
@@ -54,3 +58,80 @@ class RegisterTest(TestCase):
 
         # Check sends message
         self.assertTrue(len(get_messages(response.wsgi_request)) > 0)
+
+
+class LoginTest(TestCase):
+    def setUp(self):
+        self.next_url = reverse("user:home")
+        self.url = reverse("authentication:login") + "?next=" + self.next_url
+
+        user_pass = "str0ngP@ss"
+        self.user = baker.make(User)
+        self.user.set_password(user_pass)
+        self.user.save()
+
+        self.invalid_data = {"username_email": "invalid", "password": "wrong"}
+        self.valid_data_email = {
+            "username_email": self.user.email,
+            "password": user_pass,
+            "next": self.next_url,
+        }
+        self.valid_data_username = {
+            "username_email": self.user.username,
+            "password": user_pass,
+            "next": self.next_url,
+        }
+
+    def test_next_url_parameter(self):
+        """Should pass a 'next' parameter in context with value of
+        next url parameter.
+        """
+        response = self.client.get(self.url)
+        context = response.context
+
+        self.assertIn("next", context)
+        self.assertEqual(context["next"], self.next_url)
+
+    def test_form_type(self):
+        """Should pass a LoginForm as context form."""
+        response = self.client.get(self.url)
+        context = response.context
+
+        self.assertIn("form", context)
+        self.assertIsInstance(context["form"], LoginForm)
+
+    def test_invalid_data_form_error(self):
+        """Should not redirect or authenticate user when credentials
+        is invalid. It should add form error instead.
+        """
+        response = self.client.post(self.url, data=self.invalid_data)
+        context = response.context
+
+        # Should add error to form
+        self.assertIn("form", context)
+        self.assertIsNotNone(context["form"].errors)
+
+    def test_invalid_data_not_authenticate(self):
+        """Should not authenticate user when credentials is invalid."""
+        response = self.client.post(self.url, data=self.invalid_data)
+
+        # User should no be able to login without valid credentials
+        self.assertFalse(response.wsgi_request.user.is_authenticated)
+
+    def test_valid_data_by_username(self):
+        """Should login user by username if credentials was valid."""
+        response = self.client.post(self.url, data=self.valid_data_username)
+        # Check user is logged in
+        self.assertTrue(response.wsgi_request.user.is_authenticated)
+
+    def test_valid_data_by_email(self):
+        """Should login user by email if credentials was valid."""
+        response = self.client.post(self.url, data=self.valid_data_email)
+        # Check user is logged in
+        self.assertTrue(response.wsgi_request.user.is_authenticated)
+
+    def test_valid_data_redirect(self):
+        """Should redirect user to 'next' url if credentials was valid."""
+        response = self.client.post(self.url, data=self.valid_data_email)
+        # Check redirects to expected url
+        self.assertRedirects(response, self.next_url, target_status_code=200)
--- src/authentication/views/login.py
+++ src/authentication/views/login.py
@@ -2,10 +2,7 @@ from django.shortcuts import render, redirect
 from django.views.generic import View
 from django.http import HttpRequest
 from django.db.models import ObjectDoesNotExist
-from django.contrib.auth import (
-    authenticate, login,
-    get_user_model
-)
+from django.contrib.auth import authenticate, login, get_user_model
 
 from authentication.forms import LoginForm
 from shared.views import LogoutRequiredMixin
@@ -15,25 +12,25 @@ UserModel = get_user_model()
 
 
 class LoginView(LogoutRequiredMixin, View):
-
     def get(self, request: HttpRequest):
         form = LoginForm()
 
-        return render(request, 'authentication/login.html', {
-            'form': form,
-            'next': request.GET.get('next', '')
-        })
-
+        return render(
+            request,
+            "authentication/login.html",
+            {"form": form, "next": request.GET.get("next", "")},
+        )
 
     def post(self, request: HttpRequest):
 
         form = LoginForm(request.POST)
 
         if not form.is_valid():
-            return render(request, 'authentication/login.html',  {
-                'next': request.POST.get('next', ''),
-                'form': form
-            })
+            return render(
+                request,
+                "authentication/login.html",
+                {"next": request.POST.get("next", ""), "form": form},
+            )
 
         username_email = form.cleaned_data["username_email"]
         password = form.cleaned_data["password"]
@@ -42,7 +39,8 @@ class LoginView(LogoutRequiredMixin, View):
             # Try login by email
             user_by_email = UserModel.objects.get(email=username_email)
             user = authenticate(
-                username=user_by_email.username, password=password)
+                username=user_by_email.username, password=password
+            )
         except ObjectDoesNotExist:
             # Try login by username
             user = authenticate(username=username_email, password=password)
@@ -60,7 +58,7 @@ class LoginView(LogoutRequiredMixin, View):
             # and the url is local (starts with '/' )
             # redirect user to the next url
             next_url = request.POST.get("next")
-            if next_url and next_url.startswith('/'):
+            if next_url and next_url.startswith("/"):
                 return redirect(next_url)
 
             # If next didn't exist or wasn't local
@@ -69,7 +67,8 @@ class LoginView(LogoutRequiredMixin, View):
         else:
             form.add_error("", "نام کاربری یا رمز عبور اشتباه است")
 
-        return render(request, "authentication/login.html",  {
-            'next': request.POST.get('next', ''),
-            'form': form
-        })
+        return render(
+            request,
+            "authentication/login.html",
+            {"next": request.POST.get("next", ""), "form": form},
+        )
