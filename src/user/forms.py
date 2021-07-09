5bb0311563e4fd48a740757f8265188aa8a542cc
Alireza Jafari <alirezaja1384@gmail.com>
2021-07-10 00:35:44 +0430

    User project reformatted

--- src/user/forms.py
+++ src/user/forms.py
@@ -4,7 +4,7 @@ from django.contrib import auth
 from django.core.exceptions import ValidationError
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
-from learning.models import (Tutorial, TutorialComment, TutorialTag)
+from learning.models import Tutorial, TutorialComment, TutorialTag
 
 
 UserModel = auth.get_user_model()
@@ -13,30 +13,37 @@ UserModel = auth.get_user_model()
 class TutorialTagForm(forms.ModelForm):
     class Meta:
         model = TutorialTag
-        fields = ('title',)
+        fields = ("title",)
 
 
 class TutorialForm(forms.ModelForm):
 
-    tags = forms.CharField(required=False, label='کلمات کلیدی')
+    tags = forms.CharField(required=False, label="کلمات کلیدی")
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        # Define save_tags that returns None
+        # it will be a reference to _save_tags
+        # if form save(commit=False) be called
+        self.save_tags = lambda self: None
+
         # Add submit button
         self.helper = FormHelper()
         self.helper.add_input(
-            Submit('create_update', 'انجام', css_class='btn-success'))
+            Submit("create_update", "انجام", css_class="btn-success")
+        )
 
         # If editing model, fill tags input with current tags
-        instance: Optional[Tutorial] = kwargs.get('instance')
+        instance: Optional[Tutorial] = kwargs.get("instance")
         if instance:
             # Join tag titles with ','
-            self.fields['tags'].initial = ','.join(
-                [tag.title for tag in instance.tags.all()])
+            self.fields["tags"].initial = ",".join(
+                [tag.title for tag in instance.tags.all()]
+            )
 
     def clean_tags(self) -> list[TutorialTag]:
-        """ Convert tags from string to list of TutorialTag objects
+        """Convert tags from string to list of TutorialTag objects
             and validate them.
 
         Raises:
@@ -49,19 +56,22 @@ class TutorialForm(forms.ModelForm):
         tag_instances: list[TutorialTag] = []
 
         # Split tags by ',' and assign tag name's to tag_titles as a list
-        tag_titles = self.cleaned_data['tags'].split(
-            ',') if self.cleaned_data['tags'] else []
+        tag_titles = (
+            self.cleaned_data["tags"].split(",")
+            if self.cleaned_data["tags"]
+            else []
+        )
 
         for tag_title in tag_titles:
             # create form to validate tutorial tag
-            tag_form = TutorialTagForm(
-                {'title': tag_title})
+            tag_form = TutorialTagForm({"title": tag_title})
 
             # Validate form
             if not tag_form.is_valid():
                 # Just raise errors (without its field name)
-                raise ValidationError([tag_form.errors[field]
-                                      for field in tag_form.errors])
+                raise ValidationError(
+                    [tag_form.errors[field] for field in tag_form.errors]
+                )
             else:
                 # Set tag's tutorial
                 tag_form.instance.tutorial = self.instance
@@ -72,17 +82,18 @@ class TutorialForm(forms.ModelForm):
         return tag_instances
 
     def _save_tags(self):
-        """ Saves tutorial tags if tags changed and sets tutorial's confirm_status
-            to wating for confirm if tags changed and tutorial was confirmed before.
+        """Saves tutorial tags if tags changed and sets
+        tutorial's confirm_status to wating for confirm
+        if tags changed and tutorial was confirmed before.
         """
         # If tag has been changed
-        if 'tags' in self.changed_data:
+        if "tags" in self.changed_data:
             tutorial: Tutorial = self.instance
 
             # Delete old tags
             tutorial.tags.all().delete()
             # Create new tags
-            TutorialTag.objects.bulk_create(self.cleaned_data['tags'])
+            TutorialTag.objects.bulk_create(self.cleaned_data["tags"])
 
             # Do BEFORE_UPDATE actions for tutorial and save them
             # when tags edited
@@ -90,15 +101,15 @@ class TutorialForm(forms.ModelForm):
             tutorial.save()
 
     def save(self, commit=True) -> Tutorial:
-        """ Saves tutorial and tags(if commit=True)
+        """Saves tutorial and tags(if commit=True)
 
         Notes:
             - If you set commit=False you should call save_tags manually
               to save tags.
 
         Args:
-            commit (bool, optional): Saves tutorial object if True, else doesn't save.
-                                     Defaults to True.
+            commit (bool, optional): Saves tutorial object if True,
+                otherwise doesn't. Defaults to True.
 
         Returns:
             Tutorial: Tutorial instance created by form.
@@ -119,39 +130,51 @@ class TutorialForm(forms.ModelForm):
 
     class Meta:
         model = Tutorial
-        fields = ('title', 'short_description', 'body',
-                  'image', 'categories', 'tags', 'is_active',)
+        fields = (
+            "title",
+            "short_description",
+            "body",
+            "image",
+            "categories",
+            "tags",
+            "is_active",
+        )
 
 
 class TutorialCommentForm(forms.ModelForm):
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         # Add submit button
         self.helper = FormHelper()
         self.helper.add_input(
-            Submit('create_update', 'انجام', css_class='btn-success'))
+            Submit("create_update", "انجام", css_class="btn-success")
+        )
 
     class Meta:
         model = TutorialComment
-        fields = ('title', 'body', 'allow_reply',
-                  'notify_replies', 'is_active',)
+        fields = (
+            "title",
+            "body",
+            "allow_reply",
+            "notify_replies",
+            "is_active",
+        )
 
         help_texts = {
-            'tags': 'برای جداسازی از \'Enter\' یا \',\' استفاده کنید',
+            "tags": "برای جداسازی از 'Enter' یا ',' استفاده کنید",
         }
 
 
 class UserProfileForm(forms.ModelForm):
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         # Add submit button
         self.helper = FormHelper()
         self.helper.add_input(
-            Submit('create_update', 'انجام', css_class='btn-success'))
+            Submit("create_update", "انجام", css_class="btn-success")
+        )
 
         # Make extra_required_fields required in form
         for field in self.Meta.extra_required_fields:
@@ -159,21 +182,28 @@ class UserProfileForm(forms.ModelForm):
 
     class Meta:
         model = UserModel
-        fields = ('first_name', 'last_name', 'avatar', 'tutorials_count_goal',
-                  'comments_count_goal', 'likes_count_goal', 'views_count_goal',)
+        fields = (
+            "first_name",
+            "last_name",
+            "avatar",
+            "tutorials_count_goal",
+            "comments_count_goal",
+            "likes_count_goal",
+            "views_count_goal",
+        )
 
         extra_required_fields = (
-            'first_name',
-            'last_name',
+            "first_name",
+            "last_name",
         )
 
 
 class PasswordChangeForm(auth.forms.PasswordChangeForm):
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         # Add submit button
         self.helper = FormHelper()
         self.helper.add_input(
-            Submit('create_update', 'تغییر گذرواژه', css_class='btn-success'))
+            Submit("create_update", "تغییر گذرواژه", css_class="btn-success")
+        )
--- src/user/views/profile.py
+++ src/user/views/profile.py
@@ -1,15 +1,13 @@
 from django.forms import ModelForm
 from django.contrib import messages
-from django.contrib.auth import (
-    get_user_model, views as auth_views
-)
+from django.contrib.auth import get_user_model, views as auth_views
 from django.views.generic import UpdateView
-from django.shortcuts import (reverse, redirect)
+from django.shortcuts import reverse, redirect
 from django.urls import reverse_lazy
-from user.forms import (UserProfileForm, PasswordChangeForm)
+from user.forms import UserProfileForm, PasswordChangeForm
 
 
-SUCCESS_VIEW_NAME = 'user:home'
+SUCCESS_VIEW_NAME = "user:home"
 UserModel = get_user_model()
 
 
@@ -25,7 +23,8 @@ class UserProfileUpdateView(UpdateView):
     def form_valid(self, form: ModelForm):
         form.save()
         messages.success(
-            self.request, 'اطلاعات پروفایل شما با موفقیت ویرایش شد')
+            self.request, "اطلاعات پروفایل شما با موفقیت ویرایش شد"
+        )
         return redirect(self.get_success_url())
 
     def get_success_url(self):
@@ -34,13 +33,13 @@ class UserProfileUpdateView(UpdateView):
 
 class PasswordChangeView(auth_views.PasswordChangeView):
     form_class = PasswordChangeForm
-    success_url = reverse_lazy('user:home')
-    template_name = 'user/shared/create_update.html'
+    success_url = reverse_lazy("user:home")
+    template_name = "user/shared/create_update.html"
 
     def form_valid(self, form):
         # Call main form_valid and get its result (redirection)
         result = super().form_valid(form)
         # Message user
-        messages.success(self.request, 'گذرواژه شما با موفقیت تغییر کرد شد')
+        messages.success(self.request, "گذرواژه شما با موفقیت تغییر کرد شد")
         # Redirect user to success_url
         return result
