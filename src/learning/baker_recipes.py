269abeeec27ba225222ff69b673974e3447c4eb3
Alireza Jafari <alirezaja1384@gmail.com>
2021-07-10 23:17:59 +0430

    Tutorial model tests implemented

--- src/learning/baker_recipes.py
+++ src/learning/baker_recipes.py
@@ -1,6 +1,11 @@
 from model_bakery.recipe import Recipe
-from learning.models import Category
+from model_bakery.random_gen import gen_string
+from learning.models import Category, Tutorial
 
 
 active_category = Recipe(Category, is_active=True)
 inactive_category = Recipe(Category, is_active=False)
+
+tutorial = Recipe(
+    Tutorial, short_description=gen_string(50), body=gen_string(150)
+)
--- src/learning/tests/test_models.py
+++ src/learning/tests/test_models.py
@@ -1,13 +1,16 @@
 from django.test import TestCase
 from django.core.exceptions import ValidationError
 from model_bakery import baker
-from learning.models import Category
+from learning.models import Category, Tutorial
+from shared.models import ConfirmStatusChoices
 
 
 class TestCategory(TestCase):
     def setUp(self):
         self.category: Category = baker.make_recipe(
-            "learning.active_category", name="a test category"
+            "learning.active_category",
+            name="a test category",
+            slug="different-slug",
         )
 
     def test_auto_slug(self):
@@ -23,3 +26,47 @@ class TestCategory(TestCase):
         # set parent category to itself
         self.category.parent_category = self.category
         self.assertRaises(ValidationError, self.category.clean)
+
+
+class TestTutorial(TestCase):
+    def setUp(self):
+        tutorial: Tutorial = baker.make_recipe("learning.tutorial")
+        tutorial.body = "Changed body"
+        # confirm_status should not effect because a
+        # sensitive data (body) has been changed
+        tutorial.confirm_status = ConfirmStatusChoices.CONFIRMED
+        tutorial.save()
+
+        self.changed_tutorial = tutorial
+
+    def test_auto_slug(self):
+        """Model baker sets slug automatically but category should generate
+        slug automatically even if it set on initialization.
+        """
+        tutorial: Tutorial = baker.make_recipe(
+            "learning.tutorial",
+            title="A tutorial title",
+            slug="different-slug",
+        )
+        self.assertEqual(tutorial.slug, "a-tutorial-title")
+
+    def test_edit_is_edited(self):
+        """Should set 'is_edited=True' if a sensitive data (like body)
+        change.
+        """
+        self.assertTrue(self.changed_tutorial.is_edited)
+
+    def test_edit_last_edit_date(self):
+        """Should set 'last_edit_date' if a sensitive data (like body)
+        change.
+        """
+        self.assertIsNotNone(self.changed_tutorial.last_edit_date)
+
+    def test_edit_waiting_for_confirm(self):
+        """Should set 'confirm_status' to waiting for confirm
+        if a sensitive data (like body) change.
+        """
+        self.assertEqual(
+            self.changed_tutorial.confirm_status,
+            ConfirmStatusChoices.WAITING_FOR_CONFIRM,
+        )
