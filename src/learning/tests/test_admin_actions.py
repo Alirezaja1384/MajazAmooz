c5d50bbd70862b1d84dc944de3ee61c17a9a601d
Alireza Jafari <alirezaja1384@gmail.com>
2021-07-12 14:29:45 +0430

    TutorialCommentAdminActions tests implemented

--- src/learning/tests/test_admin_actions.py
+++ src/learning/tests/test_admin_actions.py
@@ -1,12 +1,12 @@
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock, DEFAULT, patch
 from django.test import TestCase, RequestFactory
 from model_bakery import baker
 from shared.models import ConfirmStatusChoices
-from learning.models import Tutorial
+from learning.models import Tutorial, TutorialComment
 from learning.admin import actions
 
 
-@patch("learning.admin.actions.notify_tutorial_confirm_disprove")
+@patch.object(actions, "notify_tutorial_confirm_disprove")
 class TutorialAdminActionsTest(TestCase):
     @classmethod
     def setUpClass(cls):
@@ -22,17 +22,29 @@ class TutorialAdminActionsTest(TestCase):
 
         self.queryset = Tutorial.objects.all()
 
+    def confirm_queryset(self):
+        """Confirms queryset by calling confirm_tutorial_action"""
+        actions.confirm_tutorial_action(
+            self.modeladmin,
+            self.factory.get("/admin/confirm_comments"),
+            self.queryset,
+        )
+
+    def disprove_queryset(self):
+        """Disproves queryset by calling disprove_tutorial_action"""
+        actions.disprove_tutorial_action(
+            self.modeladmin,
+            self.factory.get("/admin/disprove_comments"),
+            self.queryset,
+        )
+
     def test_confirm_action_confirm_tutorials(self, _: MagicMock):
-        """confirm_tutorial_action should confirm given tutorial queryset.
+        """Confirm action should confirm given queryset objects.
 
         Args:
             _ (MagicMock): Mocked notify_tutorial_confirm_disprove method.
         """
-        actions.confirm_tutorial_action(
-            self.modeladmin,
-            self.factory.get("/admin/confirm_tutorial"),
-            self.queryset,
-        )
+        self.confirm_queryset()
 
         # No non-confirmed object should exist
         self.assertFalse(
@@ -47,11 +59,7 @@ class TutorialAdminActionsTest(TestCase):
         Args:
             mock (MagicMock): Mocked notify_tutorial_confirm_disprove method.
         """
-        actions.confirm_tutorial_action(
-            self.modeladmin,
-            self.factory.get("/admin/confirm_tutorial"),
-            self.queryset,
-        )
+        self.confirm_queryset()
         self.assertTrue(mock.called)
 
     def test_confirm_action_message_user(self, _: MagicMock):
@@ -60,11 +68,7 @@ class TutorialAdminActionsTest(TestCase):
         Args:
             _ (MagicMock): Mocked notify_tutorial_confirm_disprove method.
         """
-        actions.confirm_tutorial_action(
-            self.modeladmin,
-            self.factory.get("/admin/confirm_tutorial"),
-            self.queryset,
-        )
+        self.confirm_queryset()
         self.assertTrue(self.modeladmin.message_user.called)
 
     def test_disprove_action_disprove_tutorials(self, _: MagicMock):
@@ -73,11 +77,7 @@ class TutorialAdminActionsTest(TestCase):
         Args:
             _ (MagicMock): Mocked notify_tutorial_confirm_disprove method.
         """
-        actions.disprove_tutorial_action(
-            self.modeladmin,
-            self.factory.get("/admin/disprove_tutorial"),
-            self.queryset,
-        )
+        self.disprove_queryset()
 
         # No non-disproved object should exist
         self.assertFalse(
@@ -92,11 +92,7 @@ class TutorialAdminActionsTest(TestCase):
         Args:
             _ (MagicMock): Mocked notify_tutorial_confirm_disprove method.
         """
-        actions.disprove_tutorial_action(
-            self.modeladmin,
-            self.factory.get("/admin/confirm_tutorial"),
-            self.queryset,
-        )
+        self.disprove_queryset()
         self.assertTrue(self.modeladmin.message_user.called)
 
     def test_disprove_action_call_notifiers(self, mock: MagicMock):
@@ -105,9 +101,122 @@ class TutorialAdminActionsTest(TestCase):
         Args:
             mock (MagicMock): Mocked notify_tutorial_confirm_disprove method.
         """
-        actions.disprove_tutorial_action(
+        self.disprove_queryset()
+        self.assertTrue(mock.called)
+
+
+@patch.multiple(
+    actions,
+    notify_tutorial_comments_reply=DEFAULT,
+    notify_tutorial_new_confirmed_comment=DEFAULT,
+    notify_tutorial_comment_confirm_disprove=DEFAULT,
+)
+class TutorialCommentAdminActionsTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.factory = RequestFactory()
+        cls.modeladmin = MagicMock()
+
+        super().setUpClass()
+
+    def setUp(self):
+        # Create 12 tutorial comments (7 confirmed and 5 disproved)
+        baker.make_recipe("learning.confirmed_tutorial_comment", _quantity=7)
+        baker.make_recipe("learning.disproved_tutorial_comment", _quantity=5)
+
+        self.queryset = TutorialComment.objects.all()
+
+    def confirm_queryset(self):
+        """Confirms queryset by calling confirm_tutorial_comment_action"""
+        actions.confirm_tutorial_comment_action(
             self.modeladmin,
-            self.factory.get("/admin/disprove_tutorial"),
+            self.factory.get("/admin/confirm_tutorial_comments"),
             self.queryset,
         )
-        self.assertTrue(mock.called)
+
+    def disprove_queryset(self):
+        """Disproves queryset by calling disprove_tutorial_comment_action"""
+        actions.disprove_tutorial_comment_action(
+            self.modeladmin,
+            self.factory.get("/admin/disprove_tutorial_comments"),
+            self.queryset,
+        )
+
+    def test_confirm_action_confirm_tutorial_comments(self, **_):
+        """Confirm action should confirm given queryset objects."""
+        self.confirm_queryset()
+        # No non-confirmed object should exist
+        self.assertFalse(
+            self.queryset.exclude(
+                confirm_status=ConfirmStatusChoices.CONFIRMED
+            ).exists()
+        )
+
+    def test_confirm_action_call_notifiers(
+        self,
+        notify_tutorial_comments_reply: MagicMock,
+        notify_tutorial_new_confirmed_comment: MagicMock,
+        notify_tutorial_comment_confirm_disprove: MagicMock,
+    ):
+        """Confirm action should call all mocked notifiers.
+
+        Args:
+            notify_tutorial_comments_reply (MagicMock): mocked
+                notify_tutorial_comments_reply.
+
+            notify_tutorial_new_confirmed_comment (MagicMock): Mocked
+                notify_tutorial_new_confirmed_comment.
+
+            notify_tutorial_comment_confirm_disprove (MagicMock): Mocked
+                notify_tutorial_comment_confirm_disprove.
+        """
+        self.confirm_queryset()
+
+        self.assertTrue(notify_tutorial_comments_reply.called)
+        self.assertTrue(notify_tutorial_new_confirmed_comment.called)
+        self.assertTrue(notify_tutorial_comment_confirm_disprove.called)
+
+    def test_confirm_action_message_user(self, **_):
+        """Confirm action should call given modeladmin's message_user()."""
+        self.confirm_queryset()
+        self.assertTrue(self.modeladmin.message_user.called)
+
+    def test_disprove_action_disprove_tutorial_comments(self, **_):
+        """Disprove action should disprove given queryset objects."""
+        self.disprove_queryset()
+        # No non-disproved object should exist
+        self.assertFalse(
+            self.queryset.exclude(
+                confirm_status=ConfirmStatusChoices.DISPROVED
+            ).exists()
+        )
+
+    def test_disprove_action_message_user(self, **_):
+        """Disprove action should call given modeladmin's message_user()."""
+        self.disprove_queryset()
+        self.assertTrue(self.modeladmin.message_user.called)
+
+    def test_disprove_action_call_notifiers(
+        self,
+        notify_tutorial_comments_reply: MagicMock,
+        notify_tutorial_new_confirmed_comment: MagicMock,
+        notify_tutorial_comment_confirm_disprove: MagicMock,
+    ):
+        """Disprove action should only call confirm_disprove notifier.
+
+        Args:
+            notify_tutorial_comments_reply (MagicMock): mocked
+                notify_tutorial_comments_reply.
+
+            notify_tutorial_new_confirmed_comment (MagicMock): Mocked
+                notify_tutorial_new_confirmed_comment.
+
+            notify_tutorial_comment_confirm_disprove (MagicMock): Mocked
+                notify_tutorial_comment_confirm_disprove.
+        """
+        self.disprove_queryset()
+        # Call confirm_disprove notifier
+        self.assertTrue(notify_tutorial_comment_confirm_disprove.called)
+        # Don't call others
+        self.assertFalse(notify_tutorial_comments_reply.called)
+        self.assertFalse(notify_tutorial_new_confirmed_comment.called)
