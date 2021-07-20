dfda57a44f4f0e278d3e7deba4a2bde553aa557b
Alireza Jafari <alirezaja1384@gmail.com>
2021-07-20 13:42:31 +0430

    TutorialConfirmDisproveNotifier tests implemented

--- src/learning/tests/test_notifications.py
+++ src/learning/tests/test_notifications.py
@@ -1,14 +1,12 @@
-import random
 from smtplib import SMTPException
 from unittest import mock
 from django.test import TestCase, RequestFactory
 from model_bakery import baker
-from shared.models import ConfirmStatusChoices
 from learning import notifications
 from learning.models import Tutorial
 
 
-class NotificationsTest(TestCase):
+class AbstractQuerysetNotifierTest(TestCase):
     class TestQuerysetNotifier(notifications.AbstractQuerysetNotifier):
         pass
 
@@ -19,21 +17,8 @@ class NotificationsTest(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        confirmed_tutorials = baker.make_recipe(
-            "learning.confirmed_tutorial", _quantity=2
-        )
-        disproved_tutorials = baker.make_recipe(
-            "learning.disproved_tutorial", _quantity=2
-        )
-        baker.make_recipe(
-            "learning.tutorial_comment",
-            tutorial=random.choice(confirmed_tutorials + disproved_tutorials),
-            _quantity=10,
-        )
-
-        cls.confirmed_disproved_tutorials = Tutorial.objects.exclude(
-            confirm_status=ConfirmStatusChoices.WAITING_FOR_CONFIRM
-        )
+        # Make tutorials
+        baker.make_recipe("learning.tutorial", _quantity=2)
 
     def setUp(self):
         self.logger_pathcher = mock.patch.object(notifications, "logger")
@@ -60,12 +45,12 @@ class NotificationsTest(TestCase):
             *args, **kwargs
         )
 
-    def test_notifier_send_email_call_send(self):
+    def test_send_email_call_send(self):
         """Should call EmailMultiAlternatives instance's send method."""
         self.send_email()
         self.assertTrue(self.email_instance_mock.send.called)
 
-    def test_notifier_send_email_smtp_exception_return_false(self):
+    def test_send_email_smtp_exception_return_false(self):
         """Should return False if EmailMultiAlternatives instance's
         save method raise SMTPException.
         """
@@ -73,7 +58,7 @@ class NotificationsTest(TestCase):
         self.email_instance_mock.send.side_effect = SMTPException()
         self.assertFalse(self.send_email())
 
-    def test_notifier_send_email_smtp_exception_log_warning(self):
+    def test_send_email_smtp_exception_log_warning(self):
         """Should log exception as warning if EmailMultiAlternatives
         instance's save method raise SMTPException.
         """
@@ -85,7 +70,7 @@ class NotificationsTest(TestCase):
         )
 
     @mock.patch.object(notifications, "render_to_string")
-    def test_notifier_send_email_render_template(
+    def test_send_email_render_template(
         self, render_to_string_mock: mock.MagicMock
     ):
         """Should call render_to_string with given template and context
@@ -102,7 +87,7 @@ class NotificationsTest(TestCase):
 
         render_to_string_mock.assert_called_with(template, context)
 
-    def test_notifier_notify_call_notify_by_email(self):
+    def test_notify_call_notify_by_email(self):
         """notify() should call notify_by_email() for each model object
         in the queryset.
         """
@@ -119,7 +104,7 @@ class NotificationsTest(TestCase):
                 notifier_instance.get_queryset().count(),
             )
 
-    def test_notifier_notify_result_success_count(self):
+    def test_notify_result_success_count(self):
         """Should correctly calculate count of successful notifications."""
         notifier_instance = self.TestQuerysetNotifier(
             self.factory.get("/"), Tutorial.objects.all()
@@ -133,7 +118,7 @@ class NotificationsTest(TestCase):
                 result.success, notifier_instance.get_queryset().count()
             )
 
-    def test_notifier_notify_result_failed_count(self):
+    def test_notify_result_failed_count(self):
         """Should correctly calculate count of failed notifications."""
         notifier_instance = self.TestQuerysetNotifier(
             self.factory.get("/"), Tutorial.objects.all()
@@ -146,6 +131,67 @@ class NotificationsTest(TestCase):
             self.assertEqual(
                 result.failed, notifier_instance.get_queryset().count()
             )
+
+    def tearDown(self):
+        self.logger_pathcher.stop()
+        self.email_cls_pathcher.stop()
+
+
+class TutorialConfirmDisproveNotifierTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.factory = RequestFactory()
+        super().setUpClass()
+
+    @classmethod
+    def setUpTestData(cls):
+        # Make tutorials
+        confirmed_tutorials: list[Tutorial] = baker.make_recipe(
+            "learning.confirmed_tutorial", _quantity=2
+        )
+        baker.make_recipe("learning.waiting_for_confirm_tutorial", _quantity=2)
+        disproved_tutorials: list[Tutorial] = baker.make_recipe(
+            "learning.disproved_tutorial", _quantity=2
+        )
+
+        cls.confirmed_disproved_tutorials = (
+            confirmed_tutorials + disproved_tutorials
+        )
+
+    def setUp(self):
+        self.logger_pathcher = mock.patch.object(notifications, "logger")
+        self.email_cls_pathcher = mock.patch.object(
+            notifications, "EmailMultiAlternatives"
+        )
+
+        self.logger_mock = self.logger_pathcher.start()
+        self.email_instance_mock = self.email_cls_pathcher.start().return_value
+
+    def test_not_include_waiting_for_confirm(self):
+        """queryset should not include waiting for confirm tutorials."""
+        notifier = notifications.TutorialConfirmDisproveNotifier(
+            self.factory.get("/"), Tutorial.objects.all()
+        )
+        qs = notifier.get_queryset().order_by()
+
+        self.assertEqual(list(qs), self.confirmed_disproved_tutorials)
+
+    def test_call_send_email(self):
+        """notify_by_email() should call send_email() once."""
+        notifier = notifications.TutorialConfirmDisproveNotifier(
+            self.factory.get("/"), Tutorial.objects.all()
+        )
+
+        with mock.patch.object(notifier, "send_email") as send_email_mock:
+            notifier.notify_by_email(mock.MagicMock())
+            send_email_mock.assert_called_once()
+
+    def test_build_url(self):
+        """build_url() should not return None."""
+        notifier = notifications.TutorialConfirmDisproveNotifier(
+            self.factory.get("/"), Tutorial.objects.all()
+        )
+        self.assertIsNotNone(notifier.build_url(notifier.queryset.first()))
 
     def tearDown(self):
         self.logger_pathcher.stop()
