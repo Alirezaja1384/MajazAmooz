3fb241a5db2f7c8a9d8d293c3c55af24991e32dd
Alireza Jafari <alirezaja1384@gmail.com>
2021-10-30 14:56:48 +0330

    * get_last_months_count_statistics:  - Query count reduced to 1 query  - Ascending (boolean) argument added  - More detailed comments added

--- src/learning/querysets/tutorial_user_relation_querysets.py
+++ src/learning/querysets/tutorial_user_relation_querysets.py
@@ -1,6 +1,6 @@
 from datetime import date
-from typing import Generator, Optional
-from django.db.models import QuerySet
+from typing import Optional
+from django.db.models import QuerySet, When, Case, Sum
 from shared.date_time import get_last_months
 from shared.statistics import MonthlyCountStatistics
 from . import get_active_confirmed_filters
@@ -14,10 +14,12 @@ class TutorialUserRelationQueryset(QuerySet):
         """
         return self.filter(get_active_confirmed_filters("tutorial"))
 
-    # TODO: Improve performance by reducing query count
     def get_last_months_count_statistics(
-        self, last_months_count: int = 5, today: Optional[date] = None
-    ) -> Generator[MonthlyCountStatistics, None, None]:
+        self,
+        last_months_count: int = 5,
+        today: Optional[date] = None,
+        ascending: bool = True,
+    ) -> list[MonthlyCountStatistics]:
         """Calculates count of objects per last given jalali months.
 
         Args:
@@ -25,19 +27,36 @@ class TutorialUserRelationQueryset(QuerySet):
                 calculate. Defaults to 5.
 
         Yields:
-            Generator[MonthlyCountStatistics, None, None]: Month's label and
+            list[MonthlyCountStatistics]: Month's label and
                 count of object for that month.
         """
-        # Convert descending months data to ascending
-        last_months = list(get_last_months(last_months_count, today))[::-1]
-
-        for month in last_months:
-            yield MonthlyCountStatistics(
-                {
-                    "label": month.label,
-                    "count": self.filter(
+        # Make a When condition for each month
+        conditions = {
+            # Calculates sum of 1s for each item created in this month
+            month.label: Sum(
+                Case(
+                    # If item is created in this month it's 1
+                    When(
                         create_date__gte=month.gregorian_start,
                         create_date__lte=month.gregorian_end,
-                    ).count(),
-                }
+                        then=1,
+                    ),
+                    # Otherwise, it's 0
+                    default=0,
+                )
             )
+            for month in get_last_months(last_months_count, today)
+        }
+
+        count_statistics: list[MonthlyCountStatistics] = [
+            # Convert aggregated count to MonthlyCountStatistics
+            MonthlyCountStatistics(
+                {"label": agg_result[0], "count": agg_result[1]}
+            )
+            for agg_result in self.aggregate(**conditions).items()
+        ]
+
+        # If ascending is True, reverse it and return (Because months are
+        # sorted in descending order, thus statistics' sort is descending too).
+        # Otherwise, return count_statistics itself.
+        return count_statistics[::-1] if ascending else count_statistics
