4f5feaad1c7de7a3686c546d424eb903ddd297d9
Alireza Jafari <alirezaja1384@gmail.com>
2022-02-01 15:34:48 +0330

    ParticipantAnswer improvements
    - Its queryset's set_correct_answers extracted
    - Its tests improved and new test added

--- src/exam/querysets/participant_answer_queryset.py
+++ src/exam/querysets/participant_answer_queryset.py
@@ -1,5 +1,6 @@
-from django.db.models import QuerySet, When, Case, Q, F
+from django.db.models import QuerySet, Subquery, OuterRef, When, Case, Q, F
 from shared.models import AnswerStatusChoices
+from exam.models import Question
 
 
 class ParticipantAnswerQuerySet(QuerySet):
@@ -25,14 +26,32 @@ class ParticipantAnswerQuerySet(QuerySet):
         """
         return self.filter(answer_status=AnswerStatusChoices.BLANK)
 
+    def set_correct_answers(self) -> int:
+        """Sets the correct answers' choices by the question.
+
+        Returns:
+            int: Updated rows count.
+        """
+        return self.update(
+            correct_answer=Subquery(
+                # Sets the correct answer by the question.
+                queryset=Question.objects.filter(
+                    pk=OuterRef("question_id")
+                ).values("correct_choice")[:1],
+            ),
+        )
+
     def set_answers_status(self) -> int:
-        """Sets the status of answers by matching the correct answers with the
-        participant's answers.
+        """Updates the correct answers (form their question) and answers' status
+        by matching the correct answers with the participant's answers.
 
         Returns:
             int: The number of answers that were set.
         """
-        return self.update(
+        # set the correct answer by the question.
+        self.set_correct_answers()
+
+        update_count = self.update(
             answer_status=Case(
                 # If user's answer is null, then set the answer status
                 # to blank.
@@ -48,5 +67,7 @@ class ParticipantAnswerQuerySet(QuerySet):
                 ),
                 # Otherwise, set the answer status to incorrect.
                 default=AnswerStatusChoices.INCORRECT,
-            )
+            ),
         )
+
+        return update_count
--- src/exam/tests/test_querysets.py
+++ src/exam/tests/test_querysets.py
@@ -1,10 +1,12 @@
 import random
+from unittest import mock
+from typing import Tuple, Union
 from model_bakery import baker
 from django.test import TestCase
-from django.db.models import QuerySet
+from django.db.models import QuerySet, F
 from shared.models import AnswerStatusChoices
-from exam.models import ParticipantAnswer
 from exam.querysets import ParticipantAnswerQuerySet
+from exam.models import ParticipantAnswer, Question
 
 
 class ParticipantAnswerQuerysetTest(TestCase):
@@ -14,56 +16,88 @@ class ParticipantAnswerQuerysetTest(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        # Using sets to avoid isEqual() issues
-        cls.incorrect_answers: ParticipantAnswer = set(
-            baker.make_recipe(
-                cls.baker_recipe,
-                correct_answer=1,
-                participant_answer=2,
-                _quantity=random.randint(1, 3),
-            )
+        question: Question = baker.make_recipe(
+            "exam.question", correct_choice=4
         )
-        cls.blank_answers: ParticipantAnswer = set(
-            baker.make_recipe(
-                cls.baker_recipe,
-                correct_answer=3,
-                participant_answer=None,
-                _quantity=random.randint(1, 3),
-            )
+
+        # * Using sets to avoid isEqual() issues
+        cls.incorrect_answer_pks: set[int] = set(
+            [
+                obj.pk
+                for obj in baker.make_recipe(
+                    cls.baker_recipe,
+                    question=question,
+                    participant_answer=2,
+                    _quantity=random.randint(1, 3),
+                )
+            ]
         )
-        cls.correct_answers: ParticipantAnswer = set(
-            baker.make_recipe(
-                cls.baker_recipe,
-                correct_answer=4,
-                participant_answer=4,
-                _quantity=random.randint(1, 3),
-            )
+        cls.blank_answer_pks: set[int] = set(
+            [
+                obj.pk
+                for obj in baker.make_recipe(
+                    cls.baker_recipe,
+                    question=question,
+                    participant_answer=None,
+                    _quantity=random.randint(1, 3),
+                )
+            ]
+        )
+        cls.correct_answer_pks: set[int] = set(
+            [
+                obj.pk
+                for obj in baker.make_recipe(
+                    cls.baker_recipe,
+                    question=question,
+                    participant_answer=4,
+                    _quantity=random.randint(1, 3),
+                )
+            ]
         )
 
         # Statuses must be set after all answers have been created.
         cls.model.objects.set_answers_status()
-        cls.model.objects.all().set_answers_status()
 
-    def get_bad_assigned_statuses(
-        self, qs: QuerySet, correct_status: AnswerStatusChoices
-    ):
-        return list(qs.exclude(answer_status=correct_status))
+    def get_wrong_assigned_status_count(
+        self, qs: QuerySet, correct_status: Union[Tuple[int, str], int]
+    ) -> int:
+        return qs.exclude(answer_status=correct_status).count()
+
+    def is_queryset_equal(self, qs: QuerySet, pks_set: set[int]):
+        """Test that the queryset contains the correct objects."""
+        self.assertEqual(set(qs.values_list("id", flat=True)), pks_set)
 
     def test_get_correct_answers(self):
         """Test that get_correct_answers returns only correct answers."""
-        filtered_objs = set(self.model.objects.get_correct_answers())
-        self.assertEqual(self.correct_answers, filtered_objs)
+        self.is_queryset_equal(
+            self.model.objects.get_correct_answers(), self.correct_answer_pks
+        )
 
     def test_get_incorrect_answers(self):
         """Test that get_incorrect_answers returns only incorrect
         answers."""
-        filtered_objs = set(self.model.objects.get_incorrect_answers())
-        self.assertEqual(self.incorrect_answers, filtered_objs)
+        self.is_queryset_equal(
+            self.model.objects.get_incorrect_answers(),
+            self.incorrect_answer_pks,
+        )
 
     def test_get_blank_answers(self):
         """Test that get_blank_answers returns only blank answers."""
-        filtered_objs = set(self.model.objects.get_blank_answers())
-        self.assertEqual(self.blank_answers, filtered_objs)
+        self.is_queryset_equal(
+            self.model.objects.get_blank_answers(), self.blank_answer_pks
+        )
+
+    @mock.patch.object(queryset, "set_correct_answers")
+    def test_set_answers_status_calls_set_correct_answers(
+        self, mock_set_currect_answers: mock.MagicMock
+    ):
+        """set_answer_status should call set_correct_answers().
+
+        Args:
+            mock_set_currect_answers: mock object for set_correct_answers()
+        """
+        self.model.objects.set_answers_status()
+        self.assertTrue(mock_set_currect_answers.called)
 
     def test_set_answers_status_correct_ones(self):
         """Test that set_answers_status sets correct answers' status
@@ -71,11 +105,11 @@ class ParticipantAnswerQuerysetTest(TestCase):
         Note: set_answers_status() has already been called in setUpTestData().
         """
         self.assertEqual(
-            self.get_bad_assigned_statuses(
+            self.get_wrong_assigned_status_count(
                 self.model.objects.get_correct_answers(),
                 AnswerStatusChoices.CORRECT,
             ),
-            [],
+            0,
         )
 
     def test_set_answers_status_incorrect_ones(self):
@@ -84,11 +118,11 @@ class ParticipantAnswerQuerysetTest(TestCase):
         Note: set_answers_status() has already been called in setUpTestData().
         """
         self.assertEqual(
-            self.get_bad_assigned_statuses(
+            self.get_wrong_assigned_status_count(
                 self.model.objects.get_incorrect_answers(),
                 AnswerStatusChoices.INCORRECT,
             ),
-            [],
+            0,
         )
 
     def test_set_answers_status_blank_ones(self):
@@ -97,9 +131,25 @@ class ParticipantAnswerQuerysetTest(TestCase):
         Note: set_answers_status() has already been called in setUpTestData().
         """
         self.assertEqual(
-            self.get_bad_assigned_statuses(
+            self.get_wrong_assigned_status_count(
                 self.model.objects.get_blank_answers(),
                 AnswerStatusChoices.BLANK,
             ),
-            [],
+            0,
+        )
+
+    def test_set_correct_answers(self):
+        """ Test that set_correct_answers sets correct_answer field correctly.
+        """
+        self.model.objects.set_correct_answers()
+
+        # The wrong assigned correct_answer fields should not exist.
+        self.assertEqual(
+            # There should not be any model object with wrong correct_answer
+            self.model.objects.select_related("question")
+            # Only include objects with correct_answer=None
+            .only("correct_answer", "question__correct_answer")
+            # Get only the objects with incorrect correct_answer field
+            .exclude(correct_answer=F("question__correct_choice")).count(),
+            0,
         )
